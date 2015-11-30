d780826f75e95d99880642aa9b7f954e3182cff0
Vivek Nair <vnair611@stanford.edu>
2015-11-30 02:10:17 -0800

    Change the indentation settings from 4 to 2 spaces

--- js/script.js
+++ js/script.js
@@ -1,36 +1,32 @@
-(function(window, document, undefined) {
+// pane elements
+var rightPane = document.getElementById('right-pane');
+// TODO: add other panes here
 
-    // pane elements
-    var rightPane = document.getElementById('right-pane');
-    // TODO: add other panes here
+// button and input elements
+// TODO: add button/input element selectors here
 
-    // button and input elements
-    // TODO: add button/input element selectors here
+/* Returns the questions stored in localStorage. */
+function getStoredQuestions() {
+  if (!localStorage.questions) {
+    // default to empty array
+    localStorage.questions = JSON.stringify([]);
+  }
 
-    /* Returns the questions stored in localStorage. */
-    function getStoredQuestions() {
-        if (!localStorage.questions) {
-            // default to empty array
-            localStorage.questions = JSON.stringify([]);
-        }
+  return JSON.parse(localStorage.questions);
+}
 
-        return JSON.parse(localStorage.questions);
-    }
+/* Store the given questions array in localStorage.
+ *
+ * Arguments:
+ * questions -- the questions array to store in localStorage
+ */
+function storeQuestions(questions) {
+  localStorage.questions = JSON.stringify(questions);
+}
 
-    /* Store the given questions array in localStorage.
-     *
-     * Arguments:
-     * questions -- the questions array to store in localStorage
-     */
-    function storeQuestions(questions) {
-        localStorage.questions = JSON.stringify(questions);
-    }
+// TODO: tasks 1-5 and one extension
 
-    // TODO: tasks 1-5 and one extension
+// display question form initially
+rightPane.appendChild(renderQuestionForm());
 
-    // display question form initially
-    rightPane.appendChild(renderQuestionForm());
-
-    // TODO: display question list initially (if there are existing questions)
-
-})(this, this.document);
+// TODO: display question list initially (if there are existing questions)
