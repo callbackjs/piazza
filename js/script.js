ed1f63803e7928198773be44c2f66ec972d9f67c
Vivek Nair <vnair611@stanford.edu>
2015-11-30 01:56:36 -0800

    Add in the templates file with the tag templating function and render functions

--- js/script.js
+++ js/script.js
@@ -7,16 +7,6 @@
     // button and input elements
     // TODO: add button/input element selectors here
 
-    // script elements that correspond to Handlebars templates
-    var questionFormTemplate = document.getElementById('question-form-template');
-    // TODO: add other script elements corresponding to templates here
-
-    // compiled Handlebars templates
-    var templates = {
-        renderQuestionForm: Handlebars.compile(questionFormTemplate.innerHTML)
-        // TODO: add other Handlebars render functions here
-    };
-
     /* Returns the questions stored in localStorage. */
     function getStoredQuestions() {
         if (!localStorage.questions) {
@@ -39,7 +29,7 @@
     // TODO: tasks 1-5 and one extension
 
     // display question form initially
-    rightPane.innerHTML = templates.renderQuestionForm();
+    rightPane.appendChild(renderQuestionForm());
 
     // TODO: display question list initially (if there are existing questions)
 
